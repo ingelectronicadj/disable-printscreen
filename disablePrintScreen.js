657a8da7b0f5330e9c59f303011d4458363540a5
Diego Mena <ingelectronicadj@gmail.com>
2021-01-06 17:23:06 -0500

    se logra deshabilitar el imprimir o exportar a PDF con el CTRL+P

--- disablePrintScreen.js
+++ disablePrintScreen.js
@@ -1,35 +1,22 @@
-// Try to include this before closing the </body>
-
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-<script>
 /** TO DISABLE SCREEN CAPTURE **/
-function removeClipboard(){
-    // Use The Clipboard API --> https://developer.mozilla.org/en-US/docs/Web/API/Navigator/clipboard
-    navigator.clipboard.writeText('');
-}
-
-// I use the sweetAlert2 library to customize alerts
-function custom_alert(icon_alert, title_alert) {
-    Swal.fire({
-        icon: icon_alert,
-        title: title_alert,
-        backdrop: true,
-        allowOutsideClick: false,
-        allowEscapeKey: false
-        });
-}
-
 // The keypress method only detects keys that generate printable characters
-// I use keyup without using obsolete methods
 document.addEventListener('keyup', (e) => {
     if (e.key == 'PrintScreen') {
-        removeClipboard();
-        custom_alert('error', 'Screenshots disabled!');
+        navigator.clipboard.writeText('');
+        alert('Capturas de pantalla deshabilitadas!');
+    }
+});
+
+/** TO DISABLE PRINTS WHIT CTRL+P **/
+document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key == 'p') {
+        alert('Esta sección no se permite imprimir o exportar en PDF');
+        e.cancelBubble = true;
+        e.preventDefault();
+        e.stopImmediatePropagation();
     }
 });
 
 /* TO DO: There are combinations that remain to be solved 
     --> Windows+Shift+S
-    --> Ctrl+P 
 */
-</script>
